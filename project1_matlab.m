dcbe5cdc134f5968556c8875485e63848bb070e2
Curnis Giovanni <35149153+GioCurnis@users.noreply.github.com>
2020-06-29 15:39:22 +0200

    updated code cholesky matlab
    now you can execute all matrices and save all results in csv file

--- project1_matlab.m
+++ project1_matlab.m
@@ -1,53 +1,57 @@
 PATH = './matrix/';
-file = "ex15.mat";
 
 dinfo = dir('./matrix/*.mat');
+outFileResult = './results/matlabResults.csv';
+title = ["matrix", "length", "nnz", "execTime", "memoryUsage", "error"];
+writematrix(title, outFileResult, 'WriteMode', 'overwrite', 'Delimiter', 'comma');
 
 for K = 1 : length(dinfo)
-  thisfilename = dinfo(K).name;  %just the name
-  fprintf( 'File #%d, "%s" \n', K, thisfilename );   %do something with the data
+    thisfilename = dinfo(K).name;  %just the name
+    fprintf( 'File #%d, "%s" \n', K, thisfilename );   %do something with the data
+
+    name = dinfo(K).name;
+
+    loadMatrix = strcat(PATH, dinfo(K).name);
+    fprintf("Computing the %s matrix\n", dinfo(K).name);
+    load(loadMatrix);
+
+    matrix = Problem.A;
+    clear Problem;
+
+    try   
+        profile clear;
+        profile('-memory','on');
+
+        length = size(matrix, 1);
+        numZeros = nnz(matrix);
+
+        givenSolution = ones(length, 1);
+        knownTerms = matrix * givenSolution;
+
+        solution = solveCholesky(matrix, knownTerms);
+
+        info = profile('info');
+        functions = {info.FunctionTable.FunctionName};
+        myfunct = find(strcmp(functions(:),'project1_matlab>solveCholesky'));
+        execTime = info.FunctionTable(myfunct).TotalTime;
+        memoryUsage = info.FunctionTable(myfunct).TotalMemAllocated;
+
+        error = norm(givenSolution - solution) / norm(givenSolution);
+        
+        %clearvars -except keepVariables result;
+    catch exception
+        disp(exception.message);
+        execTime = 0;
+        memoryUsage = 0;
+        error = 0;
+        %clearvars -except keepVariables exception;
+    end
+    toFile = [string(name) string(length) string(numZeros) string(execTime) string(memoryUsage) string(error)];
+    disp(toFile);
+    %writeOnFile = cell2mat(toFile);
+    writematrix(toFile, outFileResult, 'WriteMode', 'append', 'Delimiter', 'comma');
 end
-
-result(1).name = file;
-
-loadMatrix = strcat(PATH, file);
-fprintf("Computing the %s matrix\n", regexprep(file, '\.[^\.]*$', ''));
-load(loadMatrix);
-
-matrix = Problem.A;
-clear Problem;
-
-
-
-try   
-    profile clear;
-    profile('-memory','on');
-
-    result(1).nonzero = nnz(matrix);
-
-    givenSolution = ones(length(matrix), 1);
-    knownTerms = matrix * givenSolution;
-    
-    solution = solveCholesky(matrix, knownTerms);
-    
-    profileInfo = profile('info');
-    functionNames = {profileInfo.FunctionTable.FunctionName};
-    functionRow = find(strcmp(functionNames(:),'project1_matlab>solveCholesky'));
-    result(1).execTime = profileInfo.FunctionTable(functionRow).TotalTime;
-    result(1).memoryUsage = profileInfo.FunctionTable(functionRow).TotalMemAllocated;
-
-    result(1).error = norm(givenSolution - solution) / norm(givenSolution);
-    clearvars -except keepVariables result;
-
-    fid = fopen( './results/res.csv', 'at' );
-    fprintf(fid, '%1$s;%2$s;%3$s;%4$s;%5$s; \n',result(1).name,result(1).nonzero,result(1).execTime,result(1).memoryUsage,result(1).error);  
-    fclose(fid);
-    
-    
-catch exception
-    disp(exception.message);
-    clearvars;
-end
+clearvars -except keepVariables exception;
 
 function solution = solveCholesky(A, b)
     R = decomposition(A,'chol','lower');
