fd8bfc026be6615aee8d8d33969aa4915bcad1d3
Capra <riccardocapra0@gmail.com>
2020-07-07 15:38:05 +0200

    update py - UNSTABLE

--- project1_python.py
+++ project1_python.py
@@ -1,35 +1,44 @@
-import scipy.io as sc
+import scipy.io as sio
+import scipy.linalg as lin
 import os, fnmatch
-#import numpy as np
-#from skparse.chlmod import cholesky
+import sksparse.cholmod as skc
 import csv
 import time
-#from memory_profiler import memory_usage
+from memory_profiler import memory_usage
+from glob import glob
+import numpy as np
 
-import numpy
 
 def solveCholesky(A, b):
-    R = cholesky(A)
-    x = A*b
-    solution = R(x)
+    R = skc.analyze(A)
+    R.cholesky_inplace(A, b)
+    #R.solve_A(b)
+    #x = A*b
+    print(R)
+    solution = R(b)
     return solution
 
 if __name__ == "__main__":
-    print(numpy.__version__)
-
     execution_path = os.getcwd()
     print("percorso: ")
     print(execution_path)
-    matrices = fnmatch.filter(os.listdir(execution_path + '\matrix'),'*.mtx')
+    matrices = sorted(glob(execution_path + '\matrix\*.mat'), key=os.path.getsize)
     print(matrices)
 
     for matrix in matrices:
         #M = mmread(matrix)
+        file = sio.loadmat(matrix)
         print(execution_path + '\matrix\\' + matrix)
-        #length = M.shape[0]
-        #givenSolution = np.ones(length)
+        M = file['Problem']['A'][0][0]
+        print(type(M))
+        length = M.shape[0]
+        
+        givenSolution = np.ones(length)
+        
+        knownTerms = M*givenSolution
+        result=solveCholesky(M,knownTerms)
         #timeStart = time.time()
-        #(mem_usage, result) = memory_usage((solveCholesky,(M.tocsc(),givenSolution)),retval=True)
+        #(mem_usage, result) = memory_usage((solveCholesky,(M,knownTerms)),retval=True)
         #execTime = time.time() - timeStart
-        #error = norm(result - givenSolution) / norm(givenSolution)
+        error = lin.norm(result - givenSolution) / lin.norm(givenSolution)
         #memoryUsage = max(mem_usage)
