7ac76e21b2f3749a745e7f003327e9f35f6c5c4b
Giovanni Curnis <curnis.giovanni@gmail.com>
2020-07-08 17:20:58 +0200

    ora funziona

--- project1_python.py
+++ project1_python.py
@@ -10,10 +10,7 @@ import numpy as np
 
 
 def solveCholesky(A, b):
-    R = skc.analyze(A)
-    R.cholesky_inplace(A, b)
-    #R.solve_A(b)
-    #x = A*b
+    R = skc.cholesky(A)
     print(R)
     solution = R(b)
     return solution
@@ -22,23 +19,37 @@ if __name__ == "__main__":
     execution_path = os.getcwd()
     print("percorso: ")
     print(execution_path)
-    matrices = sorted(glob(execution_path + '\matrix\*.mat'), key=os.path.getsize)
-    print(matrices)
+    csvPATH = execution_path + "/results/pythonResults.csv"
+    with open(csvPATH, 'w') as csvfile:
+        field = ['matrix','length','execTime','memoryUsage','error']
+        writer = csv.DictWriter(csvfile, fieldnames= field)
+        writer.writeheader()
+    
+        matrices = sorted(glob(execution_path + '/matrix/*.mat'), key=os.path.getsize)
+        print(matrices)
 
-    for matrix in matrices:
-        #M = mmread(matrix)
-        file = sio.loadmat(matrix)
-        print(execution_path + '\matrix\\' + matrix)
-        M = file['Problem']['A'][0][0]
-        print(type(M))
-        length = M.shape[0]
-        
-        givenSolution = np.ones(length)
-        
-        knownTerms = M*givenSolution
-        result=solveCholesky(M,knownTerms)
-        #timeStart = time.time()
-        #(mem_usage, result) = memory_usage((solveCholesky,(M,knownTerms)),retval=True)
-        #execTime = time.time() - timeStart
-        error = lin.norm(result - givenSolution) / lin.norm(givenSolution)
-        #memoryUsage = max(mem_usage)
+        for matrix in matrices:
+            file = sio.loadmat(matrix)
+            print(file)
+            print(execution_path + '/matrix//' + matrix)
+            M = file['Problem']['A'][0][0]
+            print(type(M))
+            length = M.shape[0]
+            
+            givenSolution = np.ones(length)
+            
+            knownTerms = M*givenSolution
+            result=solveCholesky(M,knownTerms)
+            timeStart = time.time()
+            (mem_usage, result) = memory_usage((solveCholesky,(M,knownTerms)),retval=True)
+            execTime = time.time() - timeStart
+            error = lin.norm(result - givenSolution) / lin.norm(givenSolution)
+            memoryUsage = max(mem_usage)
+
+            print(execTime)
+            print(error)
+            print(memoryUsage)
+            print("*********************************************")
+            print()
+            writer.writerow({'matrix': matrix,'length':length,'execTime':execTime,'memoryUsage':memoryUsage,'error':error})
+        csvfile.close()
