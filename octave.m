7b7e1c79f47bb6acf61e72f9c74e955b74f4efdc
Capra <riccardocapra0@gmail.com>
2020-07-09 16:46:56 +0200

    Octave file created

--- octave.m
+++ octave.m
@@ -1,16 +1,20 @@
 PATH = './matrix/';
 
 dinfo = dir('./matrix/*.mat');
-outFileResult = './results/matlabResults.csv';
-title = ["matrix", "length", "nnz", "execTime", "memoryUsage", "error"];
-writematrix(title, outFileResult, 'WriteMode', 'overwrite', 'Delimiter', 'comma');
+outFileResult = './results/octaveResults.csv';
+title = ["matrix,length,nnz,execTime,error,\n"];
+
+fid = fopen (outFileResult, "a");
+%writematrix(title, outFileResult, 'WriteMode', 'overwrite', 'Delimiter', 'comma');
+fputs (fid, title);
+
 
 function solution = solveCholesky(A, b)
-    R = decomposition(A,'chol','lower');
+    R = chol(A, "lower");
     solution = R \ b;
 end
 
-for K = 1 : length(dinfo)
+for K = 1 : 1
     thisfilename = dinfo(K).name;  %just the name
     fprintf( 'File #%d, "%s" \n', K, thisfilename );   %do something with the data
 
@@ -23,23 +27,28 @@ for K = 1 : length(dinfo)
     matrix = Problem.A;
     clear Problem;
 
-    try   
-        profile clear;
-        profile('-memory','on');
+    try 
+		profile off
+		profile clear
+        profile on
+        
+        %profile('-memory','on');
 
         length = size(matrix, 1);
         numZeros = nnz(matrix);
 
         givenSolution = ones(length, 1);
         knownTerms = matrix * givenSolution;
-
-        solution = solveCholesky(matrix, knownTerms);
-
-        info = profile('info');
+		
+		solution = solveCholesky(matrix,knownTerms);
+		
+		profile off
+        
+		info = profile('info');
         functions = {info.FunctionTable.FunctionName};
-        myfunct = find(strcmp(functions(:),'project1_matlab>solveCholesky'));
+        myfunct = find(strcmp(functions(:),'solveCholesky'));
         execTime = info.FunctionTable(myfunct).TotalTime;
-        memoryUsage = info.FunctionTable(myfunct).TotalMemAllocated;
+        %memoryUsage = info.FunctionTable(myfunct).TotalMemAllocated;
 
         error = norm(givenSolution - solution) / norm(givenSolution);
         
@@ -51,14 +60,14 @@ for K = 1 : length(dinfo)
         error = 0;
         %clearvars -except keepVariables exception;
     end
-    toFile = [name, length, numZeros, execTime, memoryUsage, error];
-    disp(toFile[1]);
+    toFile = [name,",",num2str(length),",",num2str(numZeros),",",num2str(execTime),",",num2str(error),",\n" ];
+    disp(toFile);
     %writeOnFile = cell2mat(toFile);
-    writematrix(toFile, outFileResult, 'WriteMode', 'append', 'Delimiter', 'comma');
+    %writematrix(toFile, outFileResult, 'WriteMode', 'append', 'Delimiter', 'comma');
+	%csvwrite(outFileResult, toFile,"-append")
+	fputs (fid, toFile);
+	
 end
+fclose (fid);
 clearvars -except keepVariables exception;
 
-function solution = solveCholesky(A, b)
-    R = decomposition(A,'chol','lower');
-    solution = R \ b;
-end
